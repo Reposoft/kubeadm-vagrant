8a5e78625fae5dbad33fc88da212bd43106efb84
Staffan Olsson <staffan@repos.se>
2017-10-04 22:27:53 +0200

    speed up provision because apt-transport-https is already installed on xenial

--- scripts/setup-base.sh
+++ scripts/setup-base.sh
@@ -3,14 +3,6 @@
 export KUBE_VERSION="1.7.5-00"
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update
-
-apt-get install -yq \
-    ebtables \
-    ethtool \
-    docker.io \
-    apt-transport-https
-
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
@@ -18,4 +10,13 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 apt-get update
-apt-get install -yq kubelet=$KUBE_VERSION kubeadm=$KUBE_VERSION kubectl=$KUBE_VERSION
+
+apt-get update
+
+apt-get install -yq \
+    ebtables \
+    ethtool \
+    docker.io \
+    kubelet=$KUBE_VERSION \
+    kubeadm=$KUBE_VERSION \
+    kubectl=$KUBE_VERSION
