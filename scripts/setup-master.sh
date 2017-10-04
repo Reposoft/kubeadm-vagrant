93d6f3207dc86bf98d8b4df1b658639b48090211
Staffan Olsson <staffan@repos.se>
2017-10-04 22:12:09 +0200

    flannel 0.9.0 includes RBAC in the main manifest

--- scripts/setup-master.sh
+++ scripts/setup-master.sh
@@ -8,4 +8,3 @@ cp -i /etc/kubernetes/admin.conf /vagrant/kube-config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.0/Documentation/kube-flannel.yml
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.0/Documentation/kube-flannel-rbac.yml
