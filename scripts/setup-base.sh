f75498a318bdc44419dd1c201769ed8a90877be4
Staffan Olsson <staffan@repos.se>
2017-10-05 04:43:54 +0200

    Lets version specifiers be empty or contain package variants

--- scripts/setup-base.sh
+++ scripts/setup-base.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -eux
 
 # https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG.md#external-dependencies
-export DOCKER_VERSION="17.03.2~ce-0~ubuntu-xenial"
-export KUBE_VERSION="1.7.5-00"
+export DOCKER_VERSION="-ce=17.03.2~ce-0~ubuntu-xenial"
+export KUBE_VERSION="=1.7.5-00"
 export DEBIAN_FRONTEND=noninteractive
 
 curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
@@ -21,7 +21,7 @@ EOF
 apt-get update
 
 apt-get update
-apt-cache madison docker-ce | head -n 10
+apt-cache madison docker$(echo $DOCKER_VERSION | awk -F'=' '{ print $1 }')
 apt-cache madison kubeadm | head -n 5
 apt-cache madison kubelet | head -n 5
 apt-cache madison kubectl | head -n 5
@@ -29,7 +29,7 @@ apt-cache madison kubectl | head -n 5
 apt-get install -yq \
     ebtables \
     ethtool \
-    docker-ce=$DOCKER_VERSION \
-    kubelet=$KUBE_VERSION \
-    kubeadm=$KUBE_VERSION \
-    kubectl=$KUBE_VERSION
+    docker$DOCKER_VERSION \
+    kubelet$KUBE_VERSION \
+    kubeadm$KUBE_VERSION \
+    kubectl$KUBE_VERSION
