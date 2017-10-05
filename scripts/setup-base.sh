b6695ffe1dd6f1243b8803e758415b21dc5870bf
Staffan Olsson <staffan@repos.se>
2017-10-05 04:09:23 +0200

    Install 1.8.0

--- scripts/setup-base.sh
+++ scripts/setup-base.sh
@@ -1,7 +1,16 @@
 #!/bin/bash -eux
 
-export KUBE_VERSION="1.7.5-00"
+# https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG.md#external-dependencies
+export DOCKER_VERSION="17.03.2~ce-0~ubuntu-xenial"
+export KUBE_VERSION="1.8.0-00"
 export DEBIAN_FRONTEND=noninteractive
+
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable"
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
@@ -12,6 +21,7 @@ EOF
 apt-get update
 
 apt-get update
+apt-cache madison docker-ce | head -n 10
 apt-cache madison kubeadm | head -n 5
 apt-cache madison kubelet | head -n 5
 apt-cache madison kubectl | head -n 5
@@ -19,7 +29,7 @@ apt-cache madison kubectl | head -n 5
 apt-get install -yq \
     ebtables \
     ethtool \
-    docker.io \
+    docker-ce=$DOCKER_VERSION \
     kubelet=$KUBE_VERSION \
     kubeadm=$KUBE_VERSION \
     kubectl=$KUBE_VERSION
