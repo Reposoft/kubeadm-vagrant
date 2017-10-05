2c5dd02cb66d596519535520063c90ebc24e7bef
Staffan Olsson <staffan@repos.se>
2017-10-05 04:22:21 +0200

    Better way to get current conf: don't wait for prompt to overwrite

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 ```
 vagrant up
 
-# fixes Unable to connect to the server: x509: certificate signed by unknown authority
-vagrant ssh -c 'sudo cp /etc/kubernetes/admin.conf /vagrant/kube-config' master1
-
 export KUBECONFIG=$(pwd)/kube-config
 
 # wait for dns etc
--- scripts/setup-master.sh
+++ scripts/setup-master.sh
@@ -5,8 +5,8 @@ kubeadm init --apiserver-advertise-address=$1 \
   --skip-preflight-checks #https://issue.k8s.io/53356#issuecomment-333748618
 
 mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-cp -i /etc/kubernetes/admin.conf /vagrant/kube-config
+cp -uv /etc/kubernetes/admin.conf $HOME/.kube/config
+cp -uv /etc/kubernetes/admin.conf /vagrant/kube-config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.0/Documentation/kube-flannel.yml
